9b0599ad2f8b4fa1698a1715f364f2ad04a81648
Leonardo Bastos <lmbastos@Leonardos-MacBook-Pro.local>
2024-01-07 19:58:19 -0500

    pkgs

---
+++ 01-packages/before_installpackages.R
@@ -0,0 +1,29 @@
+# Installing packages 
+# (do only once per package, or again to update a previously-installed package)
+install.packages("readr") # for fast reading/writing of files
+install.packages("janitor") # for data cleaning
+install.packages("ggplot2") # for plotting
+install.packages("dplyr") # for data wrangling
+install.packages("tidyr") # for data wrangling
+install.packages("lubridate") # for date formatting
+install.packages("ggthemes") # for extra options to plot styling
+install.packages("plotly") # for interactive plots
+install.packages("gganimate") # for animated plots
+install.packages("sf") # for vector data functions
+install.packages("raster") # for raster data functions
+
+# Loading packages
+# (do everytime open a new R/RStudio window)
+library("readr")
+library("janitor")
+library("ggplot2")
+library("dplyr")
+library("tidyr")
+library("lubridate")
+library("ggthemes")
+library("plotly")
+library("gganimate")
+library("sf")
+library("raster")
+
+
