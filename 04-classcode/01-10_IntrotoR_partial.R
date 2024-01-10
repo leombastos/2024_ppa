4c6cf0cfdc066a2ff8989cdca704b24cbabb6364
Leonardo Bastos <lmbastos@Leonardos-MacBook-Pro.local>
2024-01-09 22:59:24 -0500

    intro to r partial

---
+++ 04-classcode/01-10_IntrotoR_partial.R
@@ -0,0 +1,118 @@
+## 2024 CRSS 4060/6060 - Jan 10 Agenda
+
+# Housekeeping:
+## Website (bookmark it!): https://leombastos.github.io/bastoslab/teaching/2024-ppa/2024-ppa.html
+## GitHub: material, constantly being updated 
+## YouTube: recordings  
+
+# Setting up RStudio:
+## Tools > Global Options > General > Workspace > uncheck "Restore ...", select "Never" on dropdown menu  
+## Tools > Global Options > R markdown > Show in document outline > Sections and Named Chunks
+
+
+# Learning objectives ----
+# - Become familiarized with using R and RStudio
+# - Learn about R terminology and syntax
+# - Understand different object types
+# - Create a simple data, explore it with numbers and graphics
+# - Learn about RStudio projects, create your own, set up proper sub-directories  
+
+
+# 1) R/Rstudio ----
+
+## Why R? 
+### free, 
+### runs on multiple platforms, 
+### online community and support, 
+### continuous development, 
+### reproducible research!
+
+## Why RStudio?
+### Integrates various components of an analysis
+### Colored syntax
+### Syntax suggestions
+
+## RStudio panels
+### Script  
+### Console
+### Environment
+### Files/Plots/Help
+
+# 2) R terminology ----
+# Object
+a 
+
+b 
+
+c 
+
+## Object classes
+### Data frame
+d 
+
+d
+
+### Matrices
+e 
+
+e
+
+### Lists 
+f 
+
+f
+
+## Function
+
+## Argument
+
+## Package
+## Install vs. load a package
+
+## Let's install package tibble, then load it
+
+# 3) Creating a data set, exploring it ----
+intro 
+
+intro
+  
+# Check class, summary, and structure
+
+
+# Sampling the dataset
+# First row only
+
+# First column only
+
+# Rows 1 to 3 and columns 1 to 3
+
+# Rows 1 and 3 and columns 1 and 3
+
+# 4) ggplot2 philosophy and plots ---- 
+install.packages("ggplot2")
+library(ggplot2)
+
+
+# Point
+
+# Customizing
+
+# Exporting
+
+# 5) RStudio projects ----
+## Create a folder on your Desktop named 2023_AdvPA-initials
+## Create sub-folders data, code, output
+## Create an RStudio project at the level of main folder (e.g., 2023_AdvPA-initials)
+## Create an Rmarkdwon file (just to explore, not saving it) 
+
+# 6) Assignment #1 - Play with ggplot
+# Play with scale_color_ , explore the available options, choose one different from the one in class
+# play with theme_ , explore the available options, choose one different from the one in class
+# Export your new version saving it as "plot_a1_LASTNAME.png"
+# Upload your new plot to GitHub under issue "Assignment #1"
+
+
+
+
+
+
